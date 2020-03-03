c95c4389ee8b5abb2b55789025888dac4cab9de1
issaoui_med5@yahoo.fr <Sidibouzid55>
2020-03-03 09:54:28 +0300

    add my application

---
+++ ViewController/src/beanws/RandomTocken.java
@@ -0,0 +1,18 @@
+package beanws;
+
+import java.nio.charset.Charset;
+
+import java.util.Random;
+
+public class RandomTocken {
+    public RandomTocken() {
+        super();
+    }
+    
+    public String generatingRandomString () {
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        return new String(array, Charset.forName("UTF-8"));
+         
+    }
+}
